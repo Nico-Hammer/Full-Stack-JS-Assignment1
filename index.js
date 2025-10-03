8da323d37b593df5b17908c4b9693e748964436c
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-10-03 16:26:31 -0600

    created project template

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path'); // import path to use html files
+const express = require('express'); // import express
+const app = express(); // setup variable to access express features easier
+const PORT = 3000; // choosing a port to run on
+app.set('view engine', 'html'); // telling express to render things with html
+app.use(express.static('public')); // tell express where the static files are
+
+/** 
+ * get the html file and send it to the browser when accessing the index page
+*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+/**  
+ * listening on the specified port and logging to the console that its running
+*/
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
