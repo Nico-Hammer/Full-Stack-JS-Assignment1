ceb9cd33afcb9089993dfb29f42da91f49b399d9
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-10-17 12:37:42 -0600

    v and h axis title templates working

--- public/index.js
+++ public/index.js
@@ -32,6 +32,41 @@ function drawChart() {
   */
   const options = {
     title: chartTitle,
+    hAxis: {
+      title: 'Time of Day',
+      format: 'h:mm a',
+      viewWindow: {
+        min: [7, 30, 0],
+        max: [17, 30, 0]
+      },
+      textStyle: {
+        fontSize: 14,
+        color: '#053061',
+        bold: true,
+        italic: false
+      },
+      titleTextStyle: {
+        fontSize: 18,
+        color: '#053061',
+        bold: true,
+        italic: false
+      }
+    },
+    vAxis: {
+      title: 'Rating (scale of 1-10)',
+      textStyle: {
+        fontSize: 18,
+        color: '#67001f',
+        bold: false,
+        italic: false
+      },
+      titleTextStyle: {
+        fontSize: 18,
+        color: '#67001f',
+        bold: true,
+        italic: false
+      }
+    }
   };
   document.getElementById('myChart').style.display = "block"; // make the chart visible after data is submitted
   /*
